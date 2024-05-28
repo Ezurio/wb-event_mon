cf66020737cb0f63b980b021f55f297240448898
Service Build Slave <it.devops@lairdconnect.com>
2024-05-28 15:03:23 +0000

    bump to build number 12.0.0.14

--- event_mon.c
+++ event_mon.c
@@ -23,7 +23,7 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <string.h>
 #include "sdc_sdk.h"
 
-#define BLD_NUMBER  "12.0.0.13"
+#define BLD_NUMBER  "12.0.0.14"
 
 #define EVENT_MON_VERSION_MAJOR 3
 #define EVENT_MON_VERSION_MINOR 5
