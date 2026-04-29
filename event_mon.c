bb371f188a2795b015a18e64b8b5810f64a92269
Service Build Slave <it.devops@ezurio.com>
2026-04-29 18:56:15 +0000

    Bump to build number 14.0.0.1

--- event_mon.c
+++ event_mon.c
@@ -23,7 +23,7 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #include <string.h>
 #include "sdc_sdk.h"
 
-#define BLD_NUMBER  "0.0.0.0"
+#define BLD_NUMBER  "14.0.0.1"
 
 #define EVENT_MON_VERSION_MAJOR 3
 #define EVENT_MON_VERSION_MINOR 5
